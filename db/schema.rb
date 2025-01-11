53be79dfcbaf35984d0ae62eb776a15806b0bb29
Jon <39038058+devxiongmao@users.noreply.github.com>
2025-01-10 23:21:59 -0500

    XMDEV-60: Adds attributes for users (first name, last name and drivers), and styles login page (#30)
    * XMDEV-60: Adds attributes for users (first name, last name and drivers), and styles login page
    
    * Rubocop

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[8.0].define(version: 2025_01_09_010114) do
+ActiveRecord::Schema[8.0].define(version: 2025_01_11_035548) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "pg_catalog.plpgsql"
 
@@ -55,6 +55,9 @@ ActiveRecord::Schema[8.0].define(version: 2025_01_09_010114) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.integer "role", default: 0, null: false
+    t.string "first_name"
+    t.string "last_name"
+    t.string "drivers_license"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
