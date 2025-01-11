ff570f1db0d50186037aa7cc0a458e65c94d9f85
Jon <39038058+devxiongmao@users.noreply.github.com>
2025-01-11 15:19:42 -0500

    Ensures signups have admin role (#31)
    * XMDEV-65: Ensures signups have admin role
    
    * Rubocop

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,8 +6,15 @@ class User < ApplicationRecord
 
   enum :role, { driver: 0, admin: 1 }
 
-  # Convenience methods
+  scope :drivers, -> { where(role: "driver") }
+  scope :admins, -> { where(role: "admin") }
+
+  # Helper methods
   def admin?
     role == "admin"
+  end
+
+  def driver?
+    role == "driver"
   end
 end
