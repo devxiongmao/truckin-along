b24a8e0f99d99c54060c4d7635b6d6a43061e485
Jon Psaila <jonathan_psaila@outlook.com>
2025-03-11 22:50:55 -0400

    rubocop

--- spec/policies/shipment_policy_spec.rb
+++ spec/policies/shipment_policy_spec.rb
@@ -4,26 +4,26 @@ RSpec.describe ShipmentPolicy, type: :policy do
   let(:admin) { User.new(role: 'admin') }
   let(:driver) { User.new(role: 'driver') }
   let(:customer) { User.new(role: 'customer') }
-  
+
   # User from a company for testing company-specific permissions
-  let(:company_user) { 
+  let(:company_user) {
     user = User.new(role: 'driver')
     user.company_id = 1
     user
   }
-  
-  let(:company_shipment) { 
+
+  let(:company_shipment) {
     shipment = Shipment.new
     shipment.company_id = 1
     shipment
   }
-  
-  let(:other_company_shipment) { 
+
+  let(:other_company_shipment) {
     shipment = Shipment.new
     shipment.company_id = 2
     shipment
   }
-  
+
   let(:shipment) { Shipment.new } # Mock shipment object
 
   describe 'permissions' do
@@ -80,13 +80,13 @@ RSpec.describe ShipmentPolicy, type: :policy do
       it { expect(subject.assign_shipments_to_truck?).to be false }
       it { expect(subject.initiate_delivery?).to be false }
     end
-    
+
     context 'when the user belongs to the same company as the shipment' do
       let(:user) { company_user }
-      
+
       context 'with shipment from same company' do
         subject { described_class.new(user, company_shipment) }
-        
+
         it { expect(subject.index?).to be false }
         it { expect(subject.show?).to be true }
         it { expect(subject.new?).to be false }
@@ -100,10 +100,10 @@ RSpec.describe ShipmentPolicy, type: :policy do
         it { expect(subject.assign_shipments_to_truck?).to be true }
         it { expect(subject.initiate_delivery?).to be true }
       end
-      
+
       context 'with shipment from different company' do
         subject { described_class.new(user, other_company_shipment) }
-        
+
         it { expect(subject.edit?).to be false }
         it { expect(subject.update?).to be false }
       end
