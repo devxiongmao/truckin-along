8bb090770eeaa262946e5a14942247564b3f6cec
Jon Psaila <jonathan_psaila@outlook.com>
2025-05-20 23:04:16 -0400

    specs

--- spec/jobs/geocode_delivery_shipments_job_spec.rb
+++ spec/jobs/geocode_delivery_shipments_job_spec.rb
@@ -4,18 +4,16 @@ RSpec.describe GeocodeDeliveryShipmentsJob, type: :job do
   let(:sender_address) { "1600 Amphitheatre Parkway, Mountain View, CA" }
   let(:receiver_address) { "1 Infinite Loop, Cupertino, CA" }
 
-  let!(:shipment) do
-    create(:delivery_shipment, sender_address: sender_address, receiver_address: receiver_address)
-  end
+  let (:delivery_shipment) { create(:delivery_shipment, sender_address: sender_address, receiver_address: receiver_address) }
 
   it 'updates coordinates for sender and receiver' do
-    described_class.new.perform([ shipment.id ])
+    described_class.new.perform([ delivery_shipment.id ])
 
-    shipment.reload
-    expect(shipment.sender_latitude).to eq 40.7128
-    expect(shipment.sender_longitude).to eq -74.006
-    expect(shipment.receiver_latitude).to eq 40.7128
-    expect(shipment.receiver_longitude).to eq -74.006
+    delivery_shipment.reload
+    expect(delivery_shipment.sender_latitude).to eq 40.7128
+    expect(delivery_shipment.sender_longitude).to eq -74.006
+    expect(delivery_shipment.receiver_latitude).to eq 40.7128
+    expect(delivery_shipment.receiver_longitude).to eq -74.006
   end
 
   it 'raises error if Geocoder fails, triggering Sidekiq retry' do
@@ -26,13 +24,11 @@ RSpec.describe GeocodeDeliveryShipmentsJob, type: :job do
     }.to raise_error(StandardError)
   end
 
-  it 'logs and raises when rate limited' do
+  it 'raises when rate limited' do
     allow(Geocoder).to receive(:search).with(sender_address).and_raise(Geocoder::OverQueryLimitError.new("Rate limit"))
 
-    expect(Rails.logger).to receive(:warn).with(/Rate limited by Geocoder/)
-
     expect {
-      described_class.new.perform([ shipment.id ])
+      described_class.new.perform([ delivery_shipment.id ])
     }.to raise_error(Geocoder::OverQueryLimitError)
   end
 end
