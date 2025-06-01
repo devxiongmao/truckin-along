8b443c3ff582a81092588aed78fe5fcbb6bc10c7
Jon Psaila <jonathan_psaila@outlook.com>
2025-06-01 17:47:07 -0400

    XMDEV-344: Adds phone_number attribute for companies

--- spec/factories/companies.rb
+++ spec/factories/companies.rb
@@ -2,5 +2,6 @@ FactoryBot.define do
   factory :company do
     name { "#{Faker::Company.name} #{SecureRandom.uuid}" }
     address { Faker::Address.full_address }
+    phone_number { "+1#{Faker::Number.number(digits: 10)}" }
   end
 end
