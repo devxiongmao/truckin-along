e8ffffde57fa30390f54dccea7e64d82fcf1b7bc
Jon Psaila <jonathan_psaila@outlook.com>
2025-06-18 23:31:43 -0400

    specs

--- spec/mailers/truck_mailer_spec.rb
+++ spec/mailers/truck_mailer_spec.rb
@@ -4,6 +4,8 @@ RSpec.describe TruckMailer, type: :mailer do
   describe ".send_truck_maintenance_due_email" do
     let(:user) { create(:user, role: "admin") }
     let!(:truck) { create(:truck, company: user.company) }
+    let(:mail) { described_class.send_truck_maintenance_due_email(truck) }
+
 
     it "renders the headers" do
       expect(mail.subject).to eq("Truck Maintenance Due - #{truck.display_name}")
@@ -14,11 +16,11 @@ RSpec.describe TruckMailer, type: :mailer do
     it "renders the body with the truck attributes" do
       expect(mail.body.encoded).to include(truck.make)
       expect(mail.body.encoded).to include(truck.model)
-      expect(mail.body.encoded).to include(truck.year)
+      expect(mail.body.encoded).to include(truck.year.to_s)
       expect(mail.body.encoded).to include(truck.license_plate)
       expect(mail.body.encoded).to include(truck.vin)
-      expect(mail.body.encoded).to include(truck.mileage)
-      expect(mail.body.encoded).to include(truck.weight)
+      expect(mail.body.encoded).to include(truck.mileage.to_s)
+      expect(mail.body.encoded).to include(truck.weight.to_s)
     end
   end
 end
