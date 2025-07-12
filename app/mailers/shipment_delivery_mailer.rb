79119b685cc69c67aca6f540897b3337eb8a6e46
Jon Psaila <jonathan_psaila@outlook.com>
2025-07-12 15:23:17 -0400

    XMDEV-352: Adds partially delivered email

--- app/mailers/shipment_delivery_mailer.rb
+++ app/mailers/shipment_delivery_mailer.rb
@@ -1,11 +1,15 @@
 class ShipmentDeliveryMailer < ApplicationMailer
   default from: "no-reply@example.com"
 
+  def partially_delivered_email(shipment_id)
+    @shipment = Shipment.find(shipment_id)
+    @user = @shipment.user
+    mail(to: @user.email, subject: "Your package has been delivered a partial distance!")
+  end
+
   def successfully_delivered_email(shipment_id)
     @shipment = Shipment.find(shipment_id)
-
     @user = @shipment.user
-
     mail(to: @user.email, subject: "Your package has been delivered!")
   end
 end
