102aa6d7c96b160a93ad63aba976e24ac2cc68d4
Jon Psaila <jonathan_psaila@outlook.com>
2025-08-09 14:26:11 -0400

    XMDEV-433: Adds customer creating a shipment wdio test

---
+++ features/step_definitions/customer_shipment_steps.rb
@@ -0,0 +1,40 @@
+Given('I am on the customer shipment page') do
+  visit shipments_path
+end
+
+Then('I should be on the new shipment creation form') do
+  expect(current_path).to eq(new_shipment_path)
+end
+
+Then('I should see a form with fields for shipment information') do
+  expect(page).to have_field('Name')
+  expect(page).to have_field('Sender name')
+  expect(page).to have_field('Sender address')
+  expect(page).to have_field('Receiver name')
+  expect(page).to have_field('Receiver address')
+  expect(page).to have_field('Weight (kg)')
+  expect(page).to have_field('Length (cm)')
+  expect(page).to have_field('Height (cm)')
+  expect(page).to have_field('Deliver By')
+end
+
+Then('I should be redirected to the shipment show page') do
+  expect(current_path).to eq(shipment_path(Shipment.last.id))
+end
+
+Then('I should see a success message confirming the shipment was created') do
+  expect(page).to have_content('Shipment was successfully created.')
+end
+
+Then('I should remain on the new shipment creation form') do
+  # When validation fails, the form is re-rendered at the POST path
+  expect(current_path).to eq(new_shipment_path)
+end
+
+Then('the previously entered shipment information should still be present') do
+  expect(page).to have_field('Name', with: 'Documents')
+  expect(page).to have_field('Sender name', with: "Peter Parker")
+  expect(page).to have_field('Receiver name', with: "Clark Kent")
+  expect(page).to have_field('Receiver address', with: "123 Main St, Anytown, USA")
+  expect(page).to have_field('Weight (kg)', with: '1')
+end
